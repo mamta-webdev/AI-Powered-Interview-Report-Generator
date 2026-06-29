6f1abed3d1f78e5064e89a24fc379eb5922430f6
maahi-arya <maahiarya625@gmail.com>
2026-06-29 20:51:48 +0530

    Updated app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-powered-interview-report-generator-1.onrender.com",
     credentials:true
 }))
 
